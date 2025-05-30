f87dfe44325f6155b7b1cac14c4b816fba0a0314
Maja-Nastassja Dvořáková <dvorakova.maja12@gmail.com>
2025-05-30 10:13:11 +0200

    JEDU - přiklady 1

--- src/main/java/org/example/Prikladykap5_1.java
+++ src/main/java/org/example/Prikladykap5_1.java
@@ -3,7 +3,7 @@ package org.example;
 public class Prikladykap5_1 {
     public static void main (String[] args) {
 
-        Circle kruh = new Circle(10);
+        Circle kruh = new Circle(10);  //konstruktor
 
         System.out.printf("Obvod kruhu: %.2f\n", kruh.obvod() );
         System.out.printf("Obsah kruhu: %.2f\n", kruh.obsah());
